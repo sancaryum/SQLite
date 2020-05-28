2e44b56cf28b575fbcf973694c0f121debac2e9c
sancaryum <sancaryum@gmail.com>
2020-05-28 22:15:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sqlite/kullanici.java
@@ -0,0 +1,25 @@
+package com.example.sqlite;
+
+public class kullanici {
+    String isim;
+    int id;
+
+
+
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/example/sqlite/sqlite_katmani.java
@@ -0,0 +1,23 @@
+package com.example.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class sqlite_katmani extends SQLiteOpenHelper {
+
+    public sqlite_katmani (Context c){
+
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
